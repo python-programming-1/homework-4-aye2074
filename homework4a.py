a631e32e8a07c953cf8d30001ff144a1052af533
Aiden ‪ <aye2074@naver.com>
2019-05-11 21:27:09 -0700

    add homework4a.py

---
+++ homework4a.py
@@ -0,0 +1,11 @@
+﻿vegetables = ['carrot', 'lettuce', 'onion', 'radish']
+
+
+def my_function(mylist):
+    for i in range(len(mylist[0:-1])):
+        print(mylist[i] + ',', end=' ')
+    print('and ' + mylist[-1])
+    return my_function
+
+
+my_function(vegetables)
